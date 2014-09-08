a716e536b064141a5eb23d844738f23f0c2a4f82
Nairitya Khilari <nkmann2@gmail.com>
2014-09-08 23:26:55 +0530

    Add : pthread in srtf and rr

--- header/rr.h
+++ header/rr.h
@@ -1,11 +1,13 @@
 #ifndef _HEADER_RR_H
 #define _HEADER_RR_H
 void rr(void);
+void *rr_thread(void *);
 
+static float current_time_rr=0.0;
 void rr(){
 	printf("RR started...\n");
-	float total_burst_time, curr_time = 0.0, waiting_time = 0.0, turnaround_time = 0.0;
-	int i, curr_index;
+	float total_burst_time, waiting_time = 0.0, turnaround_time = 0.0;
+	int i, curr_index, j=0;
 
 	for(i=0; i<total_process; i++){
 		total_burst_time += process[i].burst_time;
@@ -14,21 +16,27 @@ void rr(){
 
 	curr_index = 0;
 
-	for(curr_time=0; curr_time<total_burst_time;curr_time+=quantum){
-		if(process[curr_index].remaining_time <= quantum){
-			curr_time -= quantum;
-			curr_time += process[curr_index].remaining_time;
-			process[curr_index].remaining_time = 0;
-			process[curr_index].state[4] = 4;
-			process[curr_index].is_completed[4] = 1;
-			process[curr_index].turnaround_time[4] = curr_time - process[curr_index].arrival_time + quantum;
-			process[curr_index].waiting_time[4] = process[curr_index].turnaround_time[4] - process[curr_index].burst_time;
-			process[curr_index].termination_time[4] = curr_time + quantum;
+	int *thread_con = malloc(sizeof(int)*total_process);
+	memset(thread_con, sizeof(thread_con), 0);
 
-			printf("Process %s completed in %f and ended at %f, waited for %f.\n", process[curr_index].name, process[curr_index].burst_time, process[curr_index].termination_time[4], process[curr_index].waiting_time[4]);
+	pthread_t *thread = malloc(sizeof(pthread_t)*total_process);
+	memset(thread, sizeof(thread), 0);
+
+	data_sjf_non_preemptive *data = malloc(sizeof(data_sjf_non_preemptive)*total_process);
+	memset(data, sizeof(data), 0);
+
+
+	for(current_time_rr=0; current_time_rr < total_burst_time;){
+		
+		data[curr_index].smallest = curr_index;
+		data[curr_index].curr_time = current_time_rr;
+
+		thread_con[curr_index] = pthread_create(&thread[curr_index], NULL, &(rr_thread), &data[curr_index]);
+		if(thread_con[curr_index]){
+			fprintf(stderr, "Error - pthread_create() return code: %d\n", thread_con[curr_index]);
+			exit(EXIT_FAILURE);
 		}
-		else
-			process[curr_index].remaining_time -= quantum;
+		pthread_join(thread[curr_index], NULL);
 
 		i = 0;
 		while(i<total_process){
@@ -38,9 +46,12 @@ void rr(){
 				break;
 			i++;
 		}
+		// printf("current_time_rr :%f\n", current_time_rr);
+		current_time_rr += quantum;
+		// j++;
 	}
 
-	int j;
+	
 	for(j=0;j<total_process;j++){
 		waiting_time += process[j].waiting_time[4];
 		turnaround_time += process[j].termination_time[4] - process[j].arrival_time;
@@ -49,5 +60,32 @@ void rr(){
 	waiting_time /= total_process;
 	turnaround_time /= total_process;
 	printf("Avg turnaround_time is %f and avg waiting_time is %f\n\n\n", turnaround_time, waiting_time);
+}
+
+static int k = 0;
+void *rr_thread(void *data){
+	// printf("k: %d\n", k++);
+	int i, curr_index;
+	// float current_time_rr;
+	data_sjf_non_preemptive *_data = (struct _data_ *)data;
+
+	curr_index = (*_data).smallest;
+	// current_time_rr = (*_data).current_time_rr;
+
+	if(process[curr_index].remaining_time <= quantum){
+		if(process[curr_index].remaining_time ==0)return ;
+		current_time_rr -= quantum;
+		current_time_rr += process[curr_index].remaining_time;
+		process[curr_index].remaining_time = 0;
+		process[curr_index].state[4] = 4;
+		process[curr_index].is_completed[4] = 1;
+		process[curr_index].turnaround_time[4] = current_time_rr - process[curr_index].arrival_time + quantum;
+		process[curr_index].waiting_time[4] = process[curr_index].turnaround_time[4] - process[curr_index].burst_time;
+		process[curr_index].termination_time[4] = current_time_rr + quantum;
+
+		printf("Process %s completed in %f and ended at %f, waited for %f.\n", process[curr_index].name, process[curr_index].burst_time, process[curr_index].termination_time[4], process[curr_index].waiting_time[4]);
+	}
+	else
+		process[curr_index].remaining_time -= quantum;
 }
 #endif /* _HEADER_RR_H */
--- header/srtf.h
+++ header/srtf.h
@@ -2,11 +2,12 @@
 #define _HEADER_SRTF_H
 void srtf(void);
 int srtf_shortest(float);
+void *srtf_thread(void *);
 
 void srtf(){
 	printf("SRTF started...\n");
 	float total_burst_time, curr_time = 0.0, waiting_time = 0.0, turnaround_time = 0.0;
-	int i, curr_index;
+	int i, curr_index,j=0;
 
 	for(i=0; i<total_process; i++){
 		total_burst_time += process[i].burst_time;
@@ -14,23 +15,31 @@ void srtf(){
 	}
 
 	curr_index = 0;
+
+	int *thread_con = malloc(sizeof(int)*total_process);
+	memset(thread_con, sizeof(thread_con), 0);
+
+	pthread_t *thread = malloc(sizeof(pthread_t)*total_process);
+	memset(thread, sizeof(thread), 0);
+
+	data_sjf_non_preemptive *data = malloc(sizeof(data_sjf_non_preemptive)*total_process);
+	memset(data, sizeof(data), 0);
+
 	for(curr_time=0;curr_time<total_burst_time;curr_time++){
 		curr_index = srtf_shortest(curr_time);
-		process[curr_index].remaining_time -= 1;
-		// printf("index:%d\n", curr_index);
+		
+		data[curr_index].smallest = curr_index;
+		data[curr_index].curr_time = curr_time;
 
-		if(process[curr_index].remaining_time == 0){
-
-			process[curr_index].is_completed[2] = 1;
-			process[curr_index].turnaround_time[2] = curr_time + 1 - process[curr_index].arrival_time;
-			process[curr_index].waiting_time[2] = process[curr_index].turnaround_time[2] - process[curr_index].burst_time;
-			process[curr_index].termination_time[2] = curr_time + 1;
-
-			printf("Process %s completed in %f and ended at %f, waited for %f turnaround_time :%f.\n", process[curr_index].name, process[curr_index].burst_time, process[curr_index].termination_time[2], process[curr_index].waiting_time[2], process[curr_index].turnaround_time[2]);
+		thread_con[curr_index] = pthread_create(&thread[curr_index], NULL, &(srtf_thread), &data[curr_index]);
+		if(thread_con[curr_index]){
+			fprintf(stderr, "Error - pthread_create() return code: %d\n", thread_con[curr_index]);
+			exit(EXIT_FAILURE);
 		}
+		pthread_join(thread[curr_index], NULL);
+		// j++;
 	}
 
-	int j;
 	for(j=0;j<total_process;j++){
 		waiting_time += process[j].waiting_time[2];
 		turnaround_time += process[j].termination_time[2] - process[j].arrival_time;
@@ -56,5 +65,27 @@ int srtf_shortest(float curr_time){
 	}
 	// printf("Min :%d\n", min);
 	return min;
+}
+
+void *srtf_thread(void *data){
+	int curr_index;
+	float curr_time;
+	data_sjf_non_preemptive *_data = (struct _data_ *)data;
+
+	curr_index = (*_data).smallest;
+	curr_time = (*_data).curr_time;
+
+	process[curr_index].remaining_time -= 1;
+	// printf("index:%d\n", curr_index);
+
+	if(process[curr_index].remaining_time == 0){
+
+		process[curr_index].is_completed[2] = 1;
+		process[curr_index].turnaround_time[2] = curr_time + 1 - process[curr_index].arrival_time;
+		process[curr_index].waiting_time[2] = process[curr_index].turnaround_time[2] - process[curr_index].burst_time;
+		process[curr_index].termination_time[2] = curr_time + 1;
+
+		printf("Process %s completed in %f and ended at %f, waited for %f turnaround_time :%f.\n", process[curr_index].name, process[curr_index].burst_time, process[curr_index].termination_time[2], process[curr_index].waiting_time[2], process[curr_index].turnaround_time[2]);
+	}
 }
 #endif /* _HEADER_SRTF_H */
